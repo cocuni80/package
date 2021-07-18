9b3ff8821c5e03aa2987fd63aa68abdb9d8e7f9a
Jorge Riveros <christian.riveros@outlook.com>
2021-07-18 02:15:16 -0500

    ok

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CocuniRandom",
+    name="cocuni_module",
     version="0.1",
     author="Jorge Riveros",
     author_email="christian.riveros@outlook.com",
+    license='MIT',
     description='A Python package for cocuni random numbers generation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/cocuni80/package",
-    packages=setuptools.find_packages(),
+    packages=['cocunimodule'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
